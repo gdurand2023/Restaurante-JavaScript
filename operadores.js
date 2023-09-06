456a3bd290e4a7c8edf512ee32a4ca04a3c0fd25
gdurand2023 <131704538+gdurand2023@users.noreply.github.com>
2023-09-06 12:34:43 -0500

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,10 @@
+alert("Para poder observar los resultados, debes presionar F12 y visualizar la consola");
+var num1 = parseInt(prompt("Ingresa el numero 1"));
+var num2 = parseInt(prompt("Ingresa el numero 2"));
+//Operaciones
+var suma = num1 + num2;
+
+//Mostrar Resultados
+console.log("La suma de los numeros es: " + suma);
+
+
